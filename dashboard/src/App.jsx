31bd2bd7d8ccd8d1e0e22e86056c24e77f198294
Mothilragav <mothilragav@Mothilragavs-MacBook-Air.local>
2026-02-04 15:03:26 +0530

    Initial commit

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,9 +1,14 @@
 import Content1 from "./Components/Content1"
 import NavBar from "./Components/NavBar"
+import Header from "./Components/header"
 export default function App() {
   return (
-    <div className="h-full">
-      <Content1/>
+    <div className="h-full flex">
+      <NavBar />
+      <div className="flex flex-col w-full ">
+        <Header />
+        <Content1/>
+      </div>
     </div>
   )
 }
